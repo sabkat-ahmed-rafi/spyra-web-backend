861f8659c3ae52f336faee38a102d136f65a915b
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2026-03-18 19:37:32 +0600

    feat: create error middleware

---
+++ src/common/middlewares/error.middleware.js
@@ -0,0 +1,35 @@
+const { AppError, ConflictError, BadRequestError } = require("../common/errors");
+const { ValidationError, UniqueConstraintError, ForeignKeyConstraintError } = require("sequelize");
+
+module.exports = (err, _, res, _) => {
+  console.error("ERROR :", err);
+
+  let error = err;
+
+  // Handle non-AppError (unexpected errors)
+  if (!(error instanceof AppError)) {
+    // Sequelize unique constraint
+    if (err instanceof UniqueConstraintError) {
+      error = new ConflictError(err.message || "Duplicate field value");
+    }
+    // Sequelize validation error
+    else if (err instanceof ValidationError) {
+      error = new BadRequestError(err.message || "Validation failed");
+    }
+    // Sequelize foreign key error
+    else if (err instanceof ForeignKeyConstraintError) {
+      error = new BadRequestError(err.message || "Foreign key constraint failed");
+    }
+    else {
+      // fallback unknown errors
+      error = new AppError(err.message || "Internal Server Error", 500);
+    }
+  }
+
+  res.status(error.statusCode).json({
+    success: false,
+    message: error.message,
+    errorCode: error.errorCode,
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+  });
+};
