09f1df34dee22b8b4ee739e45339586055e9cbf2
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2026-03-18 16:28:18 +0600

    refactor: add limitation for urlencoded format

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(
 
 // Parsing middlewares
 app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
 // Utility middlewares
 app.use(compression()); 
